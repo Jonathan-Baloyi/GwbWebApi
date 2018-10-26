1d2eab1fa2d9b8d321eb1e422695ca06cc5750d7
Jonathan Baloyi <jonathan.baloyi@dariel.co.za>
2018-10-26 11:40:14 +0200

    DBContext, Migrations

---
+++ GwbWebApi/Context/GwbContext.cs
@@ -0,0 +1,19 @@
+﻿using GwbWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GwbWebApi.Context
+{
+    public class GwbContext : DbContext
+    {
+        public GwbContext(DbContextOptions<GwbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<ContactUs> ContactUs { get; set; }
+
+    }
+}
